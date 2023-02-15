cb978b72ed1edb9ec90eb7feedfb766c41c4f609
Alexander Ermakov <aermakov@vmware.com>
2023-02-15 16:42:28 +0200

    Hope you'll enjoy the test task =)

--- src/app/services/db.service.ts
+++ src/app/services/db.service.ts
@@ -12,7 +12,7 @@ export class DbService {
       return of(JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '[]') as Stock[]);
    }
 
-   addStockTrade(stock: Stock): Observable<never> {
+   addStockTrade(stock: Stock): Observable<null> {
       const stocks: Stock[] = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '[]');
 
       stocks.push({
@@ -21,17 +21,17 @@ export class DbService {
       });
       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(stocks));
 
-      return of();
+      return of(null);
    }
 
-   editStockTrade(stock: Stock): Observable<never> {
+   editStockTrade(stock: Stock): Observable<null> {
       const stocks: Stock[] = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY) || '[]');
       const index = stocks.findIndex(el => el.id === stock.id);
 
       stocks[index] = stock;
       localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(stocks));
 
-      return of();
+      return of(null);
    }
 
    getBalance(): Observable<Balance[]> {
